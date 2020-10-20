e8628b4586867263b1511f481fe1664710a32371
Rashmi Kattimani <rrkattimani47@gmail.com>
2020-10-20 11:25:30 +0530

    Create divisible.py

---
+++ divisible.py
@@ -0,0 +1,8 @@
+#Python Program to Print all Numbers in a Range Divisible by a Given Number
+
+lower=int(input("Enter lower range limit:"))
+upper=int(input("Enter upper range limit:"))
+n=int(input("Enter the number to be divided by:"))
+for i in range(lower,upper+1):
+    if(i%n==0):
+        print(i)
